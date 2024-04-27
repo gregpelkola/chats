36a27ee2a61400bca833db3253948ca4c0cdcfe8
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-04-27 11:10:39 -0500

    added icons and more styling

--- chats/src/components/chat/chatList/Chatlist.jsx
+++ chats/src/components/chat/chatList/Chatlist.jsx
@@ -2,7 +2,15 @@ import "./chatList.css"
 
 const Chatlist = () => {
     return (
-        <div className="chatList">Chatlist</div>
+        <div className="chatList">
+            <div className="search">
+                <div className="searchBar">
+                    <img src="./search.png" alt=""/>
+                    <input type="text" placeholder="Search"/>
+                </div>
+                <img src="./plus.png" alt=""/> 
+            </div>
+        </div>
     )
 }
 
