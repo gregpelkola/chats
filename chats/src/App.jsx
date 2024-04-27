a834ee3feb872cf59bd430b72fc44bb8b02768cf
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-04-27 09:21:25 -0500

    added components for chat, user, list, detail and started layout

--- chats/src/App.jsx
+++ chats/src/App.jsx
@@ -2,10 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import List from './components/list/List'
+import Chat from './components/chat/Chat'
+import Detail from './components/detail/Detail'
 
 const App = () => {
   return (
-    <div className="container">App</div>
+    <div className="container">
+      <List/>
+      <Chat/>
+      <Detail/>
+    </div>
   )
 }
 
