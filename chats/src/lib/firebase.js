5c7bc6f7ca82224b7433f25ec5997633ce366c83
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-04-28 10:40:58 -0500

    debugging firebase auth issue

--- chats/src/lib/firebase.js
+++ chats/src/lib/firebase.js
@@ -2,17 +2,20 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_API_KEY,
-    authDomain: "reactchat-8953e.firebaseapp.com",
-    projectId: "reactchat-8953e",
-    storageBucket: "reactchat-8953e.appspot.com",
-    messagingSenderId: "989490756392",
-    appId: "1:989490756392:web:9e54e92c7b39c531b82e1d"
+    apiKey: "import.meta.env.VITE_FIREBASE_API_KEY",
+    authDomain: "chats-7754e.firebaseapp.com",
+    projectId: "chats-7754e",
+    storageBucket: "chats-7754e.appspot.com",
+    messagingSenderId: "215509234785",
+    appId: "1:215509234785:web:2bbbca65f208cfb9676620",
+    measurementId: "G-G3TJ6NRBX3"
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export const auth = getAuth();
 export const db = getFirestore();
